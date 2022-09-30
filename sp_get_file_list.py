456175619302d6c7c60a7a52e8b2a6e6af9154ae
Santhosh Emmadi <eskguptha@gmail.com>
2022-09-30 17:50:34 +0530

    Create sp_get_file_list.py
    Get File list from Sharepoint using  Office365-REST-Python-Client

---
+++ sp_get_file_list.py
@@ -0,0 +1,46 @@
+#pip install Office365-REST-Python-Client
+#pip install git+https://github.com/vgrem/Office365-REST-Python-Client.git
+
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.files.file import File 
+
+sharepoint_base_url = 'https://{domain}.sharepoint.com/teams/sharepointname/'
+sharepoint_user = 'user'
+sharepoint_password = 'pwd'
+folder_in_sharepoint = '/teams/sharepointname/Shared%20Documents/YourFolderName/'
+
+# Sharepoint Connectivity
+auth = AuthenticationContext(sharepoint_base_url)
+auth.acquire_token_for_user(sharepoint_user, sharepoint_password)
+ctx = ClientContext(sharepoint_base_url, auth)
+web = ctx.web
+ctx.load(web)
+ctx.execute_query()
+print('Connected to SharePoint: ',web.properties['Title'])
+
+   
+#Function for getting file details in SharePoint Folder
+def folder_details(ctx, folder_in_sharepoint):
+  folder = ctx.web.get_folder_by_server_relative_url(folder_in_sharepoint)
+  fold_names = []
+  sub_folders = folder.files 
+  ctx.load(sub_folders)
+  ctx.execute_query()
+  for s_folder in sub_folders:
+    fold_names.append(s_folder.properties["Name"])
+ return fold_names
+ 
+#Getting folder details
+file_list = folder_details(ctx, folder_in_sharepoint)
+
+#Printing list of files from sharepoint folder
+print(file_list)
+
+#Reading File from SharePoint Folder
+sharepoint_file = '/teams/SustainabilityDataAccelerator/Shared%20Documents/General/Agro/2018_indirects_sustainable_sourcing_template.xlsx'
+file_response = File.open_binary(ctx, sharepoint_file)
+
+#Saving file to local
+with open("sharepointfile.csv", 'wb') as output_file:  
+    output_file.write(file_response.content)
